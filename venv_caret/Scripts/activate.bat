88505b81d0c4eec4eae5947d196f5dd5d09235c8
Wouter Selis <r0953028@student.thomasmore.be>
2023-11-05 21:54:30 +0100

    Titanic Pycaret model Wouter

--- venv_caret/Scripts/activate.bat
+++ venv_caret/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\TM_3\Cloud AI\DataWizards-1\venv_caret
+set VIRTUAL_ENV=C:\TM_3\Cloud AI\DataWizards\venv_caret
 
 if not defined PROMPT set PROMPT=$P$G
 
